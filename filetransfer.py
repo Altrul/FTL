6d651f6fdc14bc829bef93c2560df74104f525f4
Altrul <AlexanderOnko@gmail.com>
2021-12-17 17:39:26 +0300

    Fixed broadcast

--- filetransfer.py
+++ filetransfer.py
@@ -2,6 +2,17 @@ import socket
 import threading
 import os
 from time import sleep
+import time
+import netifaces
+
+def get_broadcast():
+	interfaces = netifaces.interfaces()
+	for i in interfaces:
+		a = netifaces.ifaddresses(i)
+		if 2 in a:
+			if 'broadcast' in a[2][0]:
+				broadcast = a[2][0]['broadcast']
+	return broadcast
 
 run = True
 
@@ -10,31 +21,22 @@ def find_hosts():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.settimeout(5)
 	sock.bind(('0.0.0.0', 31337))
-	while True:
+	last = time.time()
+	while time.time() - last < 5:
 		try:
-			conn, addr = sock.recv(1024)
+			conn, addr = sock.recvfrom(1024)
 			if conn == b'7LFTFND':
-				print(addr)
-				hosts.add(addr)
+				if addr not in hosts:
+					hosts.add(addr)
+					last = time.time()
 		except:
 			sock.close()
 			return list(hosts)
 
-# factories our next goal -> кориченвый/красноватый форум. сообщение будто на отрывке страницы
-# техника основы начала осознания черная обложка *чтобы начать надо* _пустая_ 11 пунктов.->подпунктыпервое - конец базы ... -> ответь на вопрос о чем ты
-# 
-# для подключнеия к инфрормационному полю достаточно сдивунуть тс
-# для увеличения сдвига можно использовать насильную ыиксацию
-# 
-# проверка.
-# книга
-# 
-
 def aknowledge():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	host, port = '127.0.0.1', 31337
+	host, port = get_broadcast(), 31337
 	while run:
-		print('HI')
 		sock.sendto(b'7LFTFND', (host, port))
 		sleep(1)
 
